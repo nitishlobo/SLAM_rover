4fb0d37bc3d7cef531861ea57a3d30a817e6ab18
theo94 <tdri932@UoA.auckland.ac.nz>
2016-05-13 11:54:58 +1200

    Added original functions to file org_functions.ino

--- Mecheng706_bot_test/Mecheng706_bot_test.ino
+++ Mecheng706_bot_test/Mecheng706_bot_test.ino
@@ -34,7 +34,7 @@ void loop(void) //main loopM
 {
   // Variable declarations
   int* side_dist;
-  
+
   static STATE machine_state = INITIALISING;
   //Finite-state machine Code
   switch (machine_state) {
@@ -84,10 +84,10 @@ void go_forward_and_align()
   // Variable declarations
   int* side_dist;
   side_dist = get_side_distances();
-  //Speed_adj is gain needed to turn motor back on course. 
+  //Speed_adj is gain needed to turn motor back on course.
   int speed_adj=2*((*(side_dist+1)) - (*(side_dist )));
-  
-  
+
+
   left_front_motor.writeMicroseconds(1500 + speed_val + speed_adj);
   left_rear_motor.writeMicroseconds(1500 + speed_val + speed_adj);
   right_rear_motor.writeMicroseconds(1500 - speed_val + speed_adj);
@@ -107,8 +107,8 @@ void align(void)
   // Variable declarations
   int* side_dist;
   side_dist = get_side_distances();
- 
-  
+
+
   // Front side sensor reading is greater than rear side sensor. Thus turn ccw.
   while (( *(side_dist) - *(side_dist+1)) > 15) {
     ccw();
@@ -167,210 +167,4 @@ STATE stopped() {
     }
   } else counter_lipo_voltage_ok = 0;
   return STOPPED;
-}
-
-void fast_flash_double_LED_builtin()
-{
-  static byte indexer = 0;
-  static unsigned long fast_flash_millis;
-  if (millis() > fast_flash_millis) {
-    indexer++;
-    if (indexer > 4) {
-      fast_flash_millis = millis() + 700;
-      digitalWrite(LED_BUILTIN, LOW);
-      indexer = 0;
-    } else {
-      fast_flash_millis = millis() + 100;
-      digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    }
-  }
-}
-
-void slow_flash_LED_builtin()
-{
-  static unsigned long slow_flash_millis;
-  if (millis() - slow_flash_millis > 2000) {
-    slow_flash_millis = millis();
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-  }
-}
-
-boolean is_battery_voltage__not_OK()
-{
-  static unsigned long previous_millis;
-
-  if (millis() - previous_millis > 500) { //500ms timed if statement to check lipo and output speed settings
-    previous_millis = millis();
-    Serial.print("Lipo level:");
-    int Lipo_level_cal;
-    //the voltage of a LiPo cell depends on its chemistry and varies from about 2.7-3.1 V (discharged) = 620(3.1V Min)
-    //to about 4.20-4.25 V (fully charged) = 820(4.1V Max)
-    Lipo_level_cal = (analogRead(A0) - 620);
-    Lipo_level_cal = Lipo_level_cal * 100;
-    Lipo_level_cal = Lipo_level_cal / 200;
-    speed_val += speed_change;
-    if (speed_val > 1000)
-      speed_val = 1000;
-    speed_change = 0;
-    Serial.print(Lipo_level_cal);
-    Serial.println("%");
-    if (Lipo_level_cal < 0) return false;
-  }
-  return false;
-}
-
-void range_and_speed_settings()
-{
-  static unsigned long previous_millis;
-
-  if (millis() - previous_millis > 500) {  //500ms timed if statement to check lipo and output speed settings
-    previous_millis = millis();
-    Serial.print("R.L. Range:");
-    Serial.println(analogRead(A1));
-    Serial.print("F.L. Range:");
-    Serial.println(analogRead(A2));
-    Serial.print("F.S. Range:");
-    Serial.println(analogRead(A3));
-    Serial.print("Side distance to wall:");
-    Serial.println(actual_dist_lr);
-    Serial.print("Front distance to wall:");
-    Serial.println(actual_dist_sr);
-  }
-
-}
-
-void read_serial_command()
-{
-  if (Serial.available()) {
-    char val = Serial.read();
-    Serial.print("Speed:");
-    Serial.print(speed_val);
-    Serial.print(" ms ");
-
-    //Perform an action depending on the command
-    switch (val) {
-      case 'w'://Move Forward
-      case 'W':
-        forward ();
-        Serial.println("Forward");
-        break;
-      case 's'://Move Backwards
-      case 'S':
-        reverse ();
-        Serial.println("Backwards");
-        break;
-      case 'q'://Turn Left
-      case 'Q':
-        strafe_left();
-        Serial.println("Strafe Left");
-        break;
-      case 'e'://Turn Right
-      case 'E':
-        strafe_right();
-        Serial.println("Strafe Right");
-        break;
-      case 'a'://Turn Right
-      case 'A':
-        ccw();
-        Serial.println("ccw");
-        break;
-      case 'd'://Turn Right
-      case 'D':
-        cw();
-        Serial.println("cw");
-        break;
-      case '-'://Turn Right
-      case '_':
-        speed_change = -100;
-        Serial.println("-100");
-        break;
-      case '=':
-      case '+':
-        speed_change = 100;
-        Serial.println("+");
-        break;
-      default:
-        stop();
-        Serial.println("stop");
-        break;
-    }
-
-  }
-
-}
-
-void disable_motors()
-{
-  left_front_motor.detach();  // detach the servo on pin left_front to the servo object
-  left_rear_motor.detach();  // detach the servo on pin left_rear to the servo object
-  right_rear_motor.detach();  // detach the servo on pin right_rear to the servo object
-  right_front_motor.detach();  // detach the servo on pin right_front to the servo object
-
-  pinMode(left_front, INPUT);
-  pinMode(left_rear, INPUT);
-  pinMode(right_rear, INPUT);
-  pinMode(right_front, INPUT);
-}
-
-void enable_motors()
-{
-  left_front_motor.attach(left_front);  // attaches the servo on pin left_front to the servo object
-  left_rear_motor.attach(left_rear);  // attaches the servo on pin left_rear to the servo object
-  right_rear_motor.attach(right_rear);  // attaches the servo on pin right_rear to the servo object
-  right_front_motor.attach(right_front);  // attaches the servo on pin right_front to the servo object
-}
-void stop() //Stop
-{
-  left_front_motor.writeMicroseconds(1500);
-  left_rear_motor.writeMicroseconds(1500);
-  right_rear_motor.writeMicroseconds(1500);
-  right_front_motor.writeMicroseconds(1500);
-}
-
-void forward()
-{
-  left_front_motor.writeMicroseconds(1500 + speed_val);
-  left_rear_motor.writeMicroseconds(1500 + speed_val);
-  right_rear_motor.writeMicroseconds(1500 - speed_val);
-  right_front_motor.writeMicroseconds(1500 - speed_val);
-}
-
-void reverse ()
-{
-  left_front_motor.writeMicroseconds(1500 - speed_val);
-  left_rear_motor.writeMicroseconds(1500 - speed_val);
-  right_rear_motor.writeMicroseconds(1500 + speed_val);
-  right_front_motor.writeMicroseconds(1500 + speed_val);
-}
-
-void ccw ()
-{
-  left_front_motor.writeMicroseconds(1500 - speed_val);
-  left_rear_motor.writeMicroseconds(1500 - speed_val);
-  right_rear_motor.writeMicroseconds(1500 - speed_val);
-  right_front_motor.writeMicroseconds(1500 - speed_val);
-}
-
-void cw ()
-{
-  left_front_motor.writeMicroseconds(1500 + speed_val);
-  left_rear_motor.writeMicroseconds(1500 + speed_val);
-  right_rear_motor.writeMicroseconds(1500 + speed_val);
-  right_front_motor.writeMicroseconds(1500 + speed_val);
-}
-
-void strafe_left ()
-{
-  left_front_motor.writeMicroseconds(1500 - speed_val);
-  left_rear_motor.writeMicroseconds(1500 + speed_val);
-  right_rear_motor.writeMicroseconds(1500 + speed_val);
-  right_front_motor.writeMicroseconds(1500 - speed_val);
-}
-
-void strafe_right ()
-{
-  left_front_motor.writeMicroseconds(1500 + speed_val);
-  left_rear_motor.writeMicroseconds(1500 - speed_val);
-  right_rear_motor.writeMicroseconds(1500 - speed_val);
-  right_front_motor.writeMicroseconds(1500 + speed_val);
 }
